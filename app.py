e414b4d68572076c9f9795ceec679bc2432cfa87
root <root@Rahat-Acer-Helios.>
2025-07-19 20:22:46 +0600

    first

---
+++ app.py
@@ -0,0 +1,25 @@
+import psycopg2
+from flask import Flask
+
+app = Flask(__name__)
+
+def connect_db():
+    return psycopg2.connect(
+        dbname="mydb",
+        user="user",
+        password="password",
+        host="db"
+    )
+
+@app.route("/")
+def home():
+    try:
+        conn = connect_db()
+        conn.close()
+        return "Connected to DB!"
+    except Exception as e:
+        return f"Failed to connect: {e}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
